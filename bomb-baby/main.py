7107ef74e1eee3c0b9fa559ff29fd42bb374d623
Sergio García <sergiogarcia@localhost.localdomain>
2021-07-20 20:25:26 +0200

    Test case 4 now also passes

--- bomb-baby/main.py
+++ bomb-baby/main.py
@@ -13,8 +13,8 @@ def solution(x, y):
     steps = 0
     current = [int(x), int(y)]
 
-    '''if (current[0] % 2 == 0) and (current[1] % 2 == 0):
-        return "impossible"'''
+    if (current[0] % 2 == 0) and (current[1] % 2 == 0):
+        return "impossible"
 
     while current != [1, 1]:
         steps = steps + 1
@@ -25,13 +25,17 @@ def solution(x, y):
         if (op_1[0] <= 1 or op_1[1] <= 1) and (op_2[0] >= 1 and op_2[1] >= 1):
             current = op_2
 
-        '''elif (op_1[0] <= 1 or op_1[1] <= 1) and (op_2[0] <= 1 or op_2[1] <= 1):
-            return "impossible"'''
+        elif (op_1[0] < 1 or op_1[1] < 1) and (op_2[0] < 1 or op_2[1] < 1):
+            return "impossible"
 
     return str(steps)
 
 
+def testsolution(x, y):
+    return "impossible"
+
+
 if __name__ == '__main__':
-    a = 4
-    b = 7
+    a = 25
+    b = 44
     print(solution(str(a), str(b)))
